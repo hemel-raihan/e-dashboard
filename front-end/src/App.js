ad0aee475d8d3dc39dd752b4d27c1462455d77a7
MIS-WD-Hemel Raihan <hemelraihan1997@gmail.com>
2024-06-03 00:24:13 +0600

    manage routing & useing components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,21 @@
-
 import './App.css';
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>E-Dashboard</h1>
+      <BrowserRouter>
+        <Nav/>
+        <Routes>
+          <Route path='/' element={<h1>this is product page</h1>} />
+          <Route path='/add-product' element={<h1>Add product page</h1>} />
+          <Route path='/update-product' element={<h1>Update product page</h1>} />
+          <Route path='/profile' element={<h1>Profile page</h1>} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
